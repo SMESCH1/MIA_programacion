54108dd943fb6ef0e754c77ea0e55638f2ed31af
SMESCH1 <sebastianmeschhenriques@gmail.com>
2025-04-08 21:57:47 -0300

    ejs semana 4

---
+++ semana_04.py
@@ -0,0 +1,21 @@
+# Ej 1
+
+def arboles_parque(nombre_archivo, nombre_parque):
+    arboles = []
+
+    for l in nombre_archivo:
+        arbol = dict(zip(id_arbol, l))
+    id_arbol in nombre_parque:
+        arboles = dict(zip(id_arbol,))
+    long,lat,id_arbol,altura_tot,diametro,inclinacio,id_especie,nombre_com,nombre_cie,tipo_folla,espacio_ve,ubicacion,nombre_fam,nombre_gen,origen,coord_x,coord_y
+
+
+
+if __name__ == "__main__":
+    nombre_archivo = "arbolado-en-espacios-verdes.csv"
+    print(f"Promedio de paquetes para completar el álbum: {promedio}")
+
+
+
+
+
