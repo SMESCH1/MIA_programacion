4f8c89005e945cee015adf60f6266a392cf97824
SMESCH1 <sebastianmeschhenriques@gmail.com>
2025-04-21 07:40:49 -0300

    add ej 2

--- semana_04.py
+++ semana_04.py
@@ -1,39 +1,33 @@
-# Ej 1
 import csv
+from collections import Counter
 
 def arboles_parque(nombre_archivo, nombre_parque):
-    arboles = []
+    arboles = {}
     with open(nombre_archivo, newline='', encoding='utf-8') as f:
         reader = csv.DictReader(f)
-        return [fila for fila in reader if fila['espacio_ve'] == nombre_parque]
+        # return [fila for fila in reader if fila['espacio_ve'] == nombre_parque]
+        for fila in reader:
+            if fila['espacio_ve'].strip().upper() == nombre_parque.strip().upper():
+                arboles[fila['id_arbol']] = fila
+    return arboles
 
-        # for fila in reader:
-        #     if fila['espacio_ve'] == nombre_parque:
-        #         print(fila)
+# funcion arbol mas popular del parque
+def arbol_mas_popular(nombre_parque, nombre_archivo):
+    arboles = arboles_parque(nombre_archivo, nombre_parque)
+    contador = Counter(fila['nombre_com'] for fila in arboles.values())
+    mas_comun = contador.most_common(1)
+    return mas_comun[0] if mas_comun else (None, 0)
 
-
-
-            # valores = linea.strip().split(',')
-            # arboles = dict(zip(columnas, valores))
-        #     print(arboles)
-        # arboles = []
-
-        # for l in nombre_archivo:
-        #     arbol = dict(zip(id_arbol, l))
-        # id_arbol in nombre_parque:
-        #     arboles = dict(zip(id_arbol,))
-        # long,lat,id_arbol,altura_tot,diametro,inclinacio,id_especie,nombre_com,nombre_cie,tipo_folla,espacio_ve,ubicacion,nombre_fam,nombre_gen,origen,coord_x,coord_y
-
-nombre_archivo = "/home/sebastian-mesch-henriques/Escritorio/MIA/MIA_programacion/arbolado-en-espacios-verdes.csv"
-arbolado = arboles_parque(nombre_archivo,'CENTENARIO')
-print(arbolado)
-
-
-if __name__ == "__main__":
-    nombre_archivo = "arbolado-en-espacios-verdes.csv"
-    print(f"")
+# funcion arboles mas altos
+def n_mas_altos(nombre_parque, n, nombre_archivo)
 
 
 
 
-
+if __name__ == '__main__':
+    parque = 'CENTENARIO'
+    nombre_archivo = 'arbolado-en-espacios-verdes.csv'
+    datos = arboles_parque(nombre_archivo, parque)
+    print(f'Total árboles en {parque}:', len(datos))
+    popular, cantidad = arbol_mas_popular(parque, nombre_archivo)
+    print(f'Árbol más popular: {popular} ({cantidad})')
